931a9615f53fa0ea870acf629ca38ca4f977f9ef
AndrewBroke <kuzukov-work@yandex.ru>
2026-03-13 11:12:37 +0700

    solve task

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+sentance = "Тут могло бы быть ваше предложение"
+words = sentance.split()
+longest_word = ""
+for word in words:
+    if len(word) > len(longest_word):
+        longest_word = word
+print("Самое длинное слово:", longest_word)
