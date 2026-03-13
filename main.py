7cd8d59262851406318d6e3bbe8ffb149024691b
AndrewBroke <kuzukov-work@yandex.ru>
2026-03-13 11:29:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 sentance = "Тут могло бы быть ваше предложение"
 words = sentance.split()
-longest_word = ""
-for word in words:
-    if len(word) > len(longest_word):
-        longest_word = word
+longest_word = max(words, key=len)
 print("Самое длинное слово:", longest_word)
